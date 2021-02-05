29963a55ad05a7006b74f486a93f49c9d714e493
Ulla <ulla.taponen@gmail.com>
2021-02-05 18:38:35 +0200

    Initial setup & communication to clients

---
+++ src/main/java/fi/taponen/discorveryserver/DiscorveryServerApplication.java
@@ -0,0 +1,15 @@
+package fi.taponen.discorveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscorveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscorveryServerApplication.class, args);
+	}
+
+}
